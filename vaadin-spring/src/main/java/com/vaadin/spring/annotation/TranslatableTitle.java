e6bb13a7841c1503ecd06d2b8069a7d315715d29
Benno Müller <benno.mueller@saxess.ag>
2016-04-15 16:26:48 +0200

    fixes #28 Make page title translatable

---
+++ vaadin-spring/src/main/java/com/vaadin/spring/annotation/TranslatableTitle.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015 The original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.vaadin.spring.annotation;
+
+import com.vaadin.ui.UI;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Defines the HTML page title for a {@link UI}, that will be translated by
+ * {@link org.springframework.context.MessageSource}.
+ *
+ * This annotation should be used instead of {@link com.vaadin.annotations.Title}. {@link com.vaadin.annotations.Title}
+ * is always prefered before {@code TranslatableTitle} to guarantee the backwards compatibility.
+ *
+ * @author Benno Müller (benno.mueller@saxess.ag)
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface TranslatableTitle {
+
+    /**
+     * Gets the HTML title as translatable message code that should be used if the UI is used on it's own.
+     *
+     * @return a message code for the page title string
+     */
+    String value();
+
+}
