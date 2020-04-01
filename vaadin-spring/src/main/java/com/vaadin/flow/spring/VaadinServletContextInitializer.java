1891db18b5e9fe71bf6ca2df47b20d10e255ecd2
Denis <denis@vaadin.com>
2020-04-01 13:09:35 +0300

    Use process method for ServletContextInitializers instead of onStartup (#591)

--- vaadin-spring/src/main/java/com/vaadin/flow/spring/VaadinServletContextInitializer.java
+++ vaadin-spring/src/main/java/com/vaadin/flow/spring/VaadinServletContextInitializer.java
@@ -78,6 +78,7 @@ import com.vaadin.flow.server.VaadinServletContext;
 import com.vaadin.flow.server.startup.AbstractRouteRegistryInitializer;
 import com.vaadin.flow.server.startup.AnnotationValidator;
 import com.vaadin.flow.server.startup.ApplicationRouteRegistry;
+import com.vaadin.flow.server.startup.ClassLoaderAwareServletContainerInitializer;
 import com.vaadin.flow.server.startup.DevModeInitializer;
 import com.vaadin.flow.server.startup.ServletVerifier;
 import com.vaadin.flow.server.startup.VaadinAppShellInitializer;
@@ -294,15 +295,15 @@ public class VaadinServletContextInitializer
 
         @SuppressWarnings("unchecked")
         private void validateAnnotations(
-                ServletContainerInitializer initializer, ServletContext context,
-                List<Class<?>> annotations) {
+                ClassLoaderAwareServletContainerInitializer initializer,
+                ServletContext context, List<Class<?>> annotations) {
 
             Stream<Class<?>> annotatedClasses = findByAnnotation(
                     getVerifiableAnnotationPackages(),
                     annotations.toArray(new Class[annotations.size()]));
             Set<Class<?>> set = annotatedClasses.collect(Collectors.toSet());
             try {
-                initializer.onStartup(set, context);
+                initializer.process(set, context);
             } catch (ServletException exception) {
                 throw new RuntimeException(
                         "Unexpected servlet exception from "
@@ -417,8 +418,16 @@ public class VaadinServletContextInitializer
                         getWebComponentPackages(), WebComponentExporter.class)
                                 .collect(Collectors.toSet());
 
-                initializer.onStartup(webComponentExporters,
-                        event.getServletContext());
+                try {
+                    initializer.process(webComponentExporters,
+                            event.getServletContext());
+                } catch (ServletException e) {
+                    throw new RuntimeException(
+                            String.format("Failed to initialize %s",
+                                    WebComponentConfigurationRegistry.class
+                                            .getSimpleName()),
+                            e);
+                }
             }
         }
     }
