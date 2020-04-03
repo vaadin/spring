6084d367dc745732775c784118f8345ebeb48f31
Mikhail Shabarov <mikhail@vaadin.com>
2020-04-03 16:07:47 +0300

    Localize startup state into DevModeInitializer

--- vaadin-spring/src/main/java/com/vaadin/flow/spring/VaadinServletContextInitializer.java
+++ vaadin-spring/src/main/java/com/vaadin/flow/spring/VaadinServletContextInitializer.java
@@ -325,7 +325,7 @@ public class VaadinServletContextInitializer
 
             if (config == null || config.isProductionMode() ||
                     !config.enableDevServer() ||
-                    isDevModeAlreadyStarted(event.getServletContext())) {
+                    isDevModeAlreadyStarted()) {
                 return;
             }
 
@@ -401,17 +401,15 @@ public class VaadinServletContextInitializer
             return customWhitelist != null && !customWhitelist.isEmpty();
         }
 
-        private boolean isDevModeAlreadyStarted(ServletContext servletContext) {
-            if (servletContext.getAttribute(
-                    DevModeInitializer.DEV_MODE_HANDLER_ALREADY_STARTED_ATTRIBUTE) == null) {
-                return false;
-            } else {
+        private boolean isDevModeAlreadyStarted() {
+            if (DevModeInitializer.isDevModeAlreadyStarted()) {
                 if (getLogger().isDebugEnabled()) {
                     getLogger().debug(
-                            "Skipped Dev Mode initialization as it has been already initialized");
+                            "Skipped DevModeHandler initialization as it has been already initialized");
                 }
                 return true;
             }
+            return false;
         }
     }
 
