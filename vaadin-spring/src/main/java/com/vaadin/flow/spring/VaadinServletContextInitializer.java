222e509ace498105ef6296b5fcc12509c27bd6f8
Mikhail Shabarov <mikhail@vaadin.com>
2020-04-03 10:54:34 +0300

    IllegalStateException when running a Vaadin Spring project on WebLogic server #502

--- vaadin-spring/src/main/java/com/vaadin/flow/spring/VaadinServletContextInitializer.java
+++ vaadin-spring/src/main/java/com/vaadin/flow/spring/VaadinServletContextInitializer.java
@@ -323,8 +323,9 @@ public class VaadinServletContextInitializer
                     .createDeploymentConfiguration(this.getClass(), event,
                             appContext);
 
-            if (config == null || config.isProductionMode()
-                    || !config.enableDevServer()) {
+            if (config == null || config.isProductionMode() ||
+                    !config.enableDevServer() ||
+                    isDevModeAlreadyStarted(event.getServletContext())) {
                 return;
             }
 
@@ -398,6 +399,19 @@ public class VaadinServletContextInitializer
 
         private boolean isWhitelistSet() {
             return customWhitelist != null && !customWhitelist.isEmpty();
+        }
+
+        private boolean isDevModeAlreadyStarted(ServletContext servletContext) {
+            if (servletContext.getAttribute(
+                    DevModeInitializer.DEV_MODE_HANDLER_ALREADY_STARTED_ATTRIBUTE) == null) {
+                return false;
+            } else {
+                if (getLogger().isDebugEnabled()) {
+                    getLogger().debug(
+                            "Skipped Dev Mode initialization as it has been already initialized");
+                }
+                return true;
+            }
         }
     }
 
