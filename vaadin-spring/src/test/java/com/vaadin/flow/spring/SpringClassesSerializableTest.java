e7c3c688958def626f96cb3373cbdf415d62fcda
Denis Anisimov <denis@vaadin.com>
2021-01-07 10:42:14 +0300

    fix: correct serializable test

--- vaadin-spring/src/test/java/com/vaadin/flow/spring/SpringClassesSerializableTest.java
+++ vaadin-spring/src/test/java/com/vaadin/flow/spring/SpringClassesSerializableTest.java
@@ -75,7 +75,7 @@ public class SpringClassesSerializableTest extends ClassesSerializableTest {
                 "com\\.vaadin\\.flow\\.spring\\.RootMappedCondition",
                 "com\\.vaadin\\.flow\\.spring\\.VaadinWebsocketEndpointExporter",
                 "com\\.vaadin\\.flow\\.spring\\.DispatcherServletRegistrationBeanConfig",
-                "com\\.vaadin\\.flow\\.spring\\.VaadinApplicaitonConfiguration",
+                "com\\.vaadin\\.flow\\.spring\\.VaadinApplicationConfiguration",
                 "com\\.vaadin\\.flow\\.spring\\.VaadinServletConfiguration",
                 "com\\.vaadin\\.flow\\.spring\\.VaadinScopesConfig",
                 "com\\.vaadin\\.flow\\.spring\\.SpringBootAutoConfiguration",
