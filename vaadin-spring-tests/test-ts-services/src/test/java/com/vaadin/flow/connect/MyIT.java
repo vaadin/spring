be77766f322182dbd3e9b99d5bfb03d33691f7b5
Artur Signell <artur@vaadin.com>
2021-01-19 10:41:43 +0200

    Add test

---
+++ vaadin-spring-tests/test-ts-services/src/test/java/com/vaadin/flow/connect/MyIT.java
@@ -0,0 +1,13 @@
+package com.vaadin.flow.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MyIT {
+
+    @Test
+    public void hello() {
+        System.out.println("Hello");
+    }
+}
