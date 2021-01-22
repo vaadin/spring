220347b0e4fccdbf8da1f6e8c03ef4315aaa9045
haijian <haijian@vaadin.com>
2021-01-22 16:57:22 +0200

    clarify the test

--- vaadin-spring-tests/test-ts-services/src/test/java/com/vaadin/flow/connect/SpringBootTestIT.java
+++ vaadin-spring-tests/test-ts-services/src/test/java/com/vaadin/flow/connect/SpringBootTestIT.java
@@ -1,13 +1,16 @@
 package com.vaadin.flow.connect;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class MyIT {
+// https://github.com/vaadin/flow/issues/9792
+public class SpringBootTestIT {
 
     @Test
     public void hello() {
-        System.out.println("Hello");
+        // To test if @SpringBootTest works
+        Assert.assertTrue(true);
     }
 }
