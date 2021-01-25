c5eb90414f0ed48e6abc07b4fa7d2af4dacd9585
haijian <haijian@vaadin.com>
2021-01-25 14:24:34 +0200

    Revert "ignore the newly added test to verify the CI build failed by it"
    This reverts commit 67d2b025cdac63caa9a663d8258736f9dfb6e1a1.

--- vaadin-spring-tests/test-ts-services/src/test/java/com/vaadin/flow/connect/SpringBootTestTest.java
+++ vaadin-spring-tests/test-ts-services/src/test/java/com/vaadin/flow/connect/SpringBootTestTest.java
@@ -1,16 +1,14 @@
 package com.vaadin.flow.connect;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
+@SpringBootTest
 // https://github.com/vaadin/flow/issues/9792
 public class SpringBootTestTest {
 
     @Test
-    @Ignore
     public void hello() {
         // To test if @SpringBootTest works
         Assert.assertTrue(true);
