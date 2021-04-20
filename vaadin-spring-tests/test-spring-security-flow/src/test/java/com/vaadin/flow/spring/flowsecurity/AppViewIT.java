66ee3b39d967c3181b00301c87250819f4d574e7
Artur Signell <artur@vaadin.com>
2021-04-20 16:44:52 +0300

    Delete all tests but one

--- vaadin-spring-tests/test-spring-security-flow/src/test/java/com/vaadin/flow/spring/flowsecurity/AppViewIT.java
+++ vaadin-spring-tests/test-spring-security-flow/src/test/java/com/vaadin/flow/spring/flowsecurity/AppViewIT.java
@@ -36,76 +36,6 @@ public class AppViewIT extends ChromeBrowserTest {
     }
 
     @Test
-    public void root_page_does_not_require_login() {
-        // when the / route is opened
-        open("");
-        Assert.assertEquals("Welcome to the Java Bank of Vaadin", $("h1").id("header").getText());
-        checkLogsForErrors();
-    }
-
-    @Test
-    public void navigate_to_private_view_prevented() {
-        // when the /private route is opened
-        open("");
-        $("a").attribute("href", "private").first().click();
-
-        // TODO Currently view access control is missing
-        Assert.assertTrue(getDriver().getPageSource().contains("Error creating bean with name"));
-    }
-
-    @Test
-    public void redirect_to_view_after_login() {
-        // when the /private route is opened
-        open("private");
-
-        // then it redirects to the default login page
-        assertPathShown("login");
-
-        // when the user logs in
-        loginUser();
-
-        // then it redirects to /private and there are no client errors
-        assertPrivatePageShown(USER_FULLNAME);
-        checkLogsForErrors();
-    }
-
-    @Test
-    public void redirect_to_resource_after_login() {
-        String contents = "Secret document for admin";
-        String path = "admin-only/secret.txt";
-        open(path);
-        loginAdmin();
-        assertPathShown(path);
-        String result = getDriver().getPageSource();
-        Assert.assertTrue(result.contains(contents));
-    }
-
-    @Test
-    public void access_restricted_to_logged_in_users() {
-        String contents = "Secret document for all logged in users";
-        String path = "all-logged-in/secret.txt";
-
-        open(path);
-        String anonResult = getDriver().getPageSource();
-        Assert.assertFalse(anonResult.contains(contents));
-        loginUser();
-        open(path);
-        String userResult = getDriver().getPageSource();
-        Assert.assertTrue(userResult.contains(contents));
-        logout();
-        open("login");
-        loginAdmin();
-        open(path);
-        String adminResult = getDriver().getPageSource();
-        Assert.assertTrue(adminResult.contains(contents));
-        logout();
-        open(path);
-        String anonResult2 = getDriver().getPageSource();
-        Assert.assertFalse(anonResult2.contains(contents));
-
-    }
-
-    @Test
     public void access_restricted_to_admin() {
         String contents = "Secret document for admin";
         String path = "admin-only/secret.txt";
@@ -126,16 +56,6 @@ public class AppViewIT extends ChromeBrowserTest {
         open(path);
         String anonResult2 = getDriver().getPageSource();
         Assert.assertFalse(anonResult2.contains(contents));
-    }
-
-    @Test
-    public void static_resources_accessible_without_login() throws Exception {
-        open("manifest.webmanifest");
-        Assert.assertTrue(getDriver().getPageSource().contains("\"name\":\"Spring Security Helper Test Project\""));
-        open("sw.js");
-        Assert.assertTrue(getDriver().getPageSource().contains("this._installAndActiveListenersAdded"));
-        open("sw-runtime-resources-precache.js");
-        Assert.assertTrue(getDriver().getPageSource().contains("self.additionalManifestEntries = ["));
     }
 
     private void assertPathShown(String path) {
