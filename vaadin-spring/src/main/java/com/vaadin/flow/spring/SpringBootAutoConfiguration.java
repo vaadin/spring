9aaffe2d4a4807c81425e026cf17ece18125ddaf
miguel <miguel@vaadin.com>
2021-06-15 16:16:03 +0200

    minor change

--- vaadin-spring/src/main/java/com/vaadin/flow/spring/SpringBootAutoConfiguration.java
+++ vaadin-spring/src/main/java/com/vaadin/flow/spring/SpringBootAutoConfiguration.java
@@ -82,7 +82,10 @@ public class SpringBootAutoConfiguration {
                 new SpringServlet(context, rootMapping), mapping);
         registration.setInitParameters(initParameters);
         registration
-                .setAsyncSupported(configurationProperties.getAsyncSupported() == null ? false : configurationProperties.getAsyncSupported());
+                .setAsyncSupported(
+                        configurationProperties.getAsyncSupported() == null ?
+                                false :
+                                configurationProperties.getAsyncSupported());
         registration.setName(
                 ClassUtils.getShortNameAsProperty(SpringServlet.class));
         registration.setLoadOnStartup(
