e4eea59799434580c49b8a36328fb8d68b6409a5
haijian <haijian@vaadin.com>
2021-08-30 09:54:14 +0300

    debug: use package.json instead of @npmpackage

--- vaadin-spring-tests/test-spring-security-fusion/src/main/java/com/vaadin/flow/spring/fusionsecurity/Application.java
+++ vaadin-spring-tests/test-spring-security-fusion/src/main/java/com/vaadin/flow/spring/fusionsecurity/Application.java
@@ -6,10 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@NpmPackage(value = "@adobe/lit-mobx",
-            version = "2.0.0-rc.4")
-@NpmPackage(value = "mobx",
-            version = "^6.1.5")
 public class Application {
 
     public static void main(String[] args) {
