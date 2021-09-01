78f5961e28f6e9f0a28af2207a3b0a54a45b7cf9
Haijian Wang <30408303+haijian-vaadin@users.noreply.github.com>
2021-09-01 10:06:45 +0300

    test: fusion test dep with @Npmpackage instead of package.json (#893)

--- vaadin-spring-tests/test-spring-security-fusion/src/main/java/com/vaadin/flow/spring/fusionsecurity/Application.java
+++ vaadin-spring-tests/test-spring-security-fusion/src/main/java/com/vaadin/flow/spring/fusionsecurity/Application.java
@@ -2,13 +2,19 @@ package com.vaadin.flow.spring.fusionsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 
+
 @PWA(name = "Spring Security Helper Test Project", shortName = "SSH Test")
 @SpringBootApplication
 @Theme("spring-security-test-app")
+@NpmPackage(value = "@adobe/lit-mobx",
+            version = "1.0.1")
+@NpmPackage(value = "mobx",
+            version = "6.2.0")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
