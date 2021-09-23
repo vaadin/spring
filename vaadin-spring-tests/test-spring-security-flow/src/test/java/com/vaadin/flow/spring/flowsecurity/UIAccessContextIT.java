bec48b8b8a35168fef4a75b6a8d77b9564beb265
Artur Signell <artur@vaadin.com>
2021-09-23 20:50:02 +0300

    Fix test

--- vaadin-spring-tests/test-spring-security-flow/src/test/java/com/vaadin/flow/spring/flowsecurity/UIAccessContextIT.java
+++ vaadin-spring-tests/test-spring-security-flow/src/test/java/com/vaadin/flow/spring/flowsecurity/UIAccessContextIT.java
@@ -4,59 +4,56 @@ import com.vaadin.flow.component.button.testbench.ButtonElement;
 import com.vaadin.flow.component.login.testbench.LoginFormElement;
 import com.vaadin.flow.component.login.testbench.LoginOverlayElement;
 import com.vaadin.testbench.HasElementQuery;
-import com.vaadin.testbench.TestBench;
 import com.vaadin.testbench.TestBenchElement;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 public class UIAccessContextIT extends AbstractIT {
 
-    private WebDriver createHeadlessChromeDriver() {
-        ChromeOptions headlessOptions = new ChromeOptions();
-        headlessOptions.addArguments("--headless", "--disable-gpu");
-        return TestBench.createDriver(new ChromeDriver(headlessOptions));
-    }
-
     @Test
-    public void securityContextSetForUIAccess() {
+    public void securityContextSetForUIAccess() throws Exception {
         String expectedUserBalance = "Hello John the User, your bank account balance is $10000.00.";
         String expectedAdminBalance = "Hello Emma the Admin, your bank account balance is $200000.00.";
 
-        open("private");
-        loginUser();
-        TestBenchElement balance = $("span").id("balanceText");
-        Assert.assertEquals(expectedUserBalance, balance.getText());
+        WebDriver adminBrowser = getDriver();
+        try {
+            super.setup();
+            open("private");
+            loginUser();
+            TestBenchElement balance = $("span").id("balanceText");
+            Assert.assertEquals(expectedUserBalance, balance.getText());
 
-        WebDriver adminBrowser = createHeadlessChromeDriver();
-        open("private", adminBrowser);
-        HasElementQuery adminContext = new HasElementQuery() {
+            open("private", adminBrowser);
+            HasElementQuery adminContext = new HasElementQuery() {
 
-            @Override
-            public SearchContext getContext() {
-                return adminBrowser;
-            }
+                @Override
+                public SearchContext getContext() {
+                    return adminBrowser;
+                }
 
-        };
-        loginAdmin(adminContext);
-        TestBenchElement adminBalance = adminContext.$("span")
-                .id("balanceText");
-        Assert.assertEquals(expectedAdminBalance, adminBalance.getText());
+            };
+            loginAdmin(adminContext);
+            TestBenchElement adminBalance = adminContext.$("span")
+                    .id("balanceText");
+            Assert.assertEquals(expectedAdminBalance, adminBalance.getText());
 
-        ButtonElement sendRefresh = $(ButtonElement.class).id("sendRefresh");
-        sendRefresh.click();
-        Assert.assertEquals(expectedUserBalance, balance.getText());
-        Assert.assertEquals(expectedAdminBalance, adminBalance.getText());
+            ButtonElement sendRefresh = $(ButtonElement.class)
+                    .id("sendRefresh");
+            sendRefresh.click();
+            Assert.assertEquals(expectedUserBalance, balance.getText());
+            Assert.assertEquals(expectedAdminBalance, adminBalance.getText());
 
-        ButtonElement adminSendRefresh = adminContext.$(ButtonElement.class)
-                .id("sendRefresh");
-        adminSendRefresh.click();
-        Assert.assertEquals(expectedUserBalance, balance.getText());
-        Assert.assertEquals(expectedAdminBalance, adminBalance.getText());
+            ButtonElement adminSendRefresh = adminContext.$(ButtonElement.class)
+                    .id("sendRefresh");
+            adminSendRefresh.click();
+            Assert.assertEquals(expectedUserBalance, balance.getText());
+            Assert.assertEquals(expectedAdminBalance, adminBalance.getText());
+        } finally {
+            adminBrowser.quit();
+        }
     }
 
     private void loginAdmin(HasElementQuery adminContext) {
