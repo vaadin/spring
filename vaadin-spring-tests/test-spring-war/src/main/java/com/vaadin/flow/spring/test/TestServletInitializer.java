84bfac257eec75baea0cb57070434458b1435263
Artur Signell <artur@vaadin.com>
2022-01-19 14:39:56 +0200

    fix: Remove dependency to outdated oauth2 library

--- vaadin-spring-tests/test-spring-war/src/main/java/com/vaadin/flow/spring/test/TestServletInitializer.java
+++ vaadin-spring-tests/test-spring-war/src/main/java/com/vaadin/flow/spring/test/TestServletInitializer.java
@@ -18,35 +18,13 @@ package com.vaadin.flow.spring.test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @SpringBootApplication
-@EnableAuthorizationServer
 @Configuration
 @EnableWebSecurity
-public class TestServletInitializer extends SpringBootServletInitializer
-        implements AuthorizationServerConfigurer {
+@Import(DummyOAuth2Server.class)
+public class TestServletInitializer extends SpringBootServletInitializer {
 
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer security)
-            throws Exception {
-        // doesn't need any impl
-    }
-
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients)
-            throws Exception {
-        clients.inMemory().build();
-    }
-
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints)
-            throws Exception {
-        // doesn't need any impl
-    }
 }
