d3890db99e3ddf1db9a08b450d11ea455b1d31f8
Marco Collovati <marco@vaadin.com>
2023-08-09 12:34:39 +0200

    test: Upgrade polymer components version

--- vaadin-spring-tests/test-spring-common/src/main/java/com/vaadin/flow/spring/test/DoubleNpmAnnotationHolder.java
+++ vaadin-spring-tests/test-spring-common/src/main/java/com/vaadin/flow/spring/test/DoubleNpmAnnotationHolder.java
@@ -9,7 +9,7 @@ import com.vaadin.flow.component.dependency.NpmPackage;
  *
  * @see com.vaadin.flow.spring.test.DoubleNpmAnnotationView
  */
-@NpmPackage(value = "@polymer/paper-input", version = "3.0.2")
-@NpmPackage(value = "@polymer/paper-checkbox", version = "3.0.1")
+@NpmPackage(value = "@polymer/paper-input", version = "3.2.1")
+@NpmPackage(value = "@polymer/paper-checkbox", version = "3.1.0")
 public class DoubleNpmAnnotationHolder {
 }
